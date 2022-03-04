4c5aa9755b66a21d3d4ab8d99c17b393f4bea3b8
BayuDC <dev.bayudc@gmail.com>
2022-03-04 14:44:21 +0700

    Load data for daily report form

--- SimpleRetail/Forms/Report/DailyReportForm.cs
+++ SimpleRetail/Forms/Report/DailyReportForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace SimpleRetail.Forms.Report {
@@ -16,7 +17,23 @@ namespace SimpleRetail.Forms.Report {
         }
 
         private void DailyReportForm_Load(object sender, EventArgs e) {
+            var date = DateTime.Now.Date;
+            var transactionCount = _db.Transactions.Where(t => t.Date == date).Count();
+            var transactionProducts = (
+                from transactionProduct in _db.TransactionProducts
+                join transaction in _db.Transactions
+                on transactionProduct.TransactionId equals transaction.Id
+                where (transaction.Date == date)
+                select new {
+                    transactionProduct.Quantity,
+                    transactionProduct.Price
+                }
+            ).ToList();
 
+            lblDate.Text = date.ToString("d");
+            lblTransactions.Text = transactionCount.ToString();
+            lblProducts.Text = transactionProducts.Sum(tp => tp.Quantity).ToString();
+            lblIncome.Text = transactionProducts.Sum(tp => tp.Price).ToString();
         }
     }
 }
